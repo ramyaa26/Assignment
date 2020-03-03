4f306e8eb031d0a6a77dc8c8a27d93ba409bce50
RamyaRamakrishnan <RamyaRamakrishnan@DESKTOP-D6VO719.in.ibm.com>
2020-03-03 22:36:56 +0530

    initial version

--- final-framework-testng/src/com/training/pom/LoginPOM.java
+++ final-framework-testng/src/com/training/pom/LoginPOM.java
@@ -13,14 +13,14 @@ public class LoginPOM {
 		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(id="login")
+	@FindBy(id="input-username")
 	private WebElement userName; 
 	
-	@FindBy(id="password")
+	@FindBy(id="input-password")
 	private WebElement password;
 	
-	@FindBy(id="formLogin_submitAuth")
-	private WebElement loginBtn; 
+	@FindBy(xpath="//*[@class='btn btn-primary']")
+	private WebElement loginBtn;  
 	
 	public void sendUserName(String userName) {
 		this.userName.clear();
